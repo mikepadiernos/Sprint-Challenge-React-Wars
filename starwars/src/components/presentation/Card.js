1481296f7bb005600d409a22c144dce107aa94dc
Michael Padiernos <mike.padiernos@gmail.com>
2019-11-22 23:28:33 -0500

    Work: - finished presentation of cards using css grids - finished presentation of a card basing some colors from SWAPI

--- starwars/src/components/presentation/Card.js
+++ starwars/src/components/presentation/Card.js
@@ -5,14 +5,23 @@ export const Card = styled.article`
 	background-color: #fff;
 	border-radius: 4px;
 	text-align: left;
+	overflow: hidden;
 `;
 
 export const CardHeader = styled.div`
 	margin: 0;
-	padding: 1.5rem; 
+	padding: 1.5rem;
+	background-color: #1c1e22;
+	color: #ffe300;
+	h2 {
+		margin: 0;
+	}
 `;
 
 export const CardBody = styled.div`
 	margin: 0;
-	padding: 1.5rem;
+	padding: 1.5rem 1.5rem 0;
+	p {
+	 margin: 0 0 1.5rem;
+	}
 `;
