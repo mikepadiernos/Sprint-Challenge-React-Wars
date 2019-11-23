49d0108c2bf8b7641c4052744de1a29696c01a37
Michael Padiernos <mike.padiernos@gmail.com>
2019-11-23 13:26:34 -0500

    Work: - styled cards better

--- starwars/src/components/presentation/Cards.js
+++ starwars/src/components/presentation/Cards.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import styled from "styled-components";
 
+export const CardsContainer = styled.div`
+	padding: 0 2rem;
+`;
+
 export const Cards = styled.section`
 	display: grid;
 	grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
